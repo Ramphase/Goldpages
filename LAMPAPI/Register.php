1e03f8f3d6e3cc78c2f83125761d79af8325630c
Gavin <97692441+Ramphase@users.noreply.github.com>
2022-02-14 23:32:07 -0500

    Update Register.php

--- LAMPAPI/Register.php
+++ LAMPAPI/Register.php
@@ -1,10 +1,5 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Credentials: true ");
-header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
-header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
-
 //Place user values into variables
 $inData = getRequestInfo();
 $firstName = $inData["firstName"];
